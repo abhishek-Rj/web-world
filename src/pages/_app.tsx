4b8013f938bc65121f15e565877c6964389e1902
abhishek-Rj <mahitomaki96@gmail.com>
2025-07-22 10:55:36 +0530

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import "@/styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
-import { pressStart2P } from "@/fonts";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <ToastContainer
-        toastStyle={{ fontFamily: pressStart2P.className }}
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
---
+++ src/pages/selectAvatar.tsx
@@ -0,0 +1,3 @@
+export default function SelectAvatar() {
+  return <div>SelectAvatar</div>;
+}
