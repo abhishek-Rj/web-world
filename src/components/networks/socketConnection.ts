185485b945d1b6f93d3b7862bf1f795ba87a2f61
abhishek-Rj <mahitomaki96@gmail.com>
2026-03-31 04:16:42 +0530

    upt: socket Connection update

--- src/components/networks/socketConnection.ts
+++ src/components/networks/socketConnection.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socketConnection = io("http://localhost:4000", {
+const socketConnection = io(import.meta.env.VITE_BASE_URL, {
   transports: ["websocket"],
   autoConnect: false,
   reconnection: false,
